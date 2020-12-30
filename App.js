2288b98c32293212581bb6e04802fa77c9c37a2d
ysuran92 <martinyanez420@gmail.com>
2020-12-30 11:41:57 -0300

    idk

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { render } from 'react-dom';
+import { StyleSheet, Text, View, Button, Alert, document } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text> Open up App.js to start working on your app! Bastard! </Text>
+      <Text></Text>
+      <Button
+  onPress={() => getDateTime()}
+  title="LOG TIME"
+  color="#841584"
+  accessibilityLabel="Log time."
+/>
       <StatusBar style="auto" />
     </View>
   );
@@ -19,3 +26,18 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+const getDateTime=()=>{
+  
+  var dt = new Date();
+
+  var date = dt.getDate();
+  var month = dt.getMonth() + 1;
+  var year = dt.getFullYear();
+  var hours = dt.getHours();
+  var minutes = (dt.getMinutes() < 10 ? '0' : '') + dt.getMinutes();
+  var seconds = (dt.getSeconds() < 10 ? '0' : '') + dt.getSeconds();
+
+  Alert.alert(hours + ':' + minutes + ':' + seconds + ' (' + date + '-' + month + '-' + year + ')');
+  return date + '-' + month + '-' + year + ' ' + hours + ':' + minutes + ':' + seconds;
+}
